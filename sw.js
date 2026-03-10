b5f06ee7134787660d299b146dcd72ad733090b1
antoneli1982 <antoneli.engenheiro@gmail.com>
2026-03-09 21:11:47 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // LION 5W2H - Service Worker v5 - bust:1773091936
-const CACHE = 'LION-5w2h-v13-1773101024';
+const CACHE = 'LION-5w2h-v14-1773101418';
 const FILES = ['/', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
